2ff2ab913e2e35457e2459e7ef34e35493606fce
Matt Cherry <mttchrry@gmail.com>
2015-04-01 17:23:17 -0400

    Got GetJSON to work initially, still need to parse response

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 
 
-function displayArticles(data) {
-  console.log('JSON data is : '+ data);
-  // var items = [];
-  // $.each( data, function( key, val) {
-  //   items.push("<li id='" + key + "'>" + val + "</li>" );
-  // });
+// function displayArticles(data) {
+//   console.log('JSON data is : '+ data);
+//   // var items = [];
+//   // $.each( data, function( key, val) {
+//   //   items.push("<li id='" + key + "'>" + val + "</li>" );
+//   // });
 
-  // $( "<ul/>", {
-  //   "class": "my-new-list",
-  //   html: items.join( "" )
-  // }).appendTo( "body" );
-  //   }
-}
+//   // $( "<ul/>", {
+//   //   "class": "my-new-list",
+//   //   html: items.join( "" )
+//   // }).appendTo( "body" );
+//   //   }
+// }
 
 
 function loadData() {
@@ -28,21 +28,27 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-    $('.bgimg').remove();
+    //$('.bgimg').remove();
     // YOUR CODE GOES HERE!
     var inputStreet = $('#street').val();
     var inputCity = $('#city').val();
+    console.log(inputStreet);
+    console.log(inputCity);
     var address = inputStreet + ', ' + inputCity;
     var streetimgaddress = 'https://maps.googleapis.com/maps/api/streetview?location=' +
-         address + '&size=600x400'
+         address + '&size=600x400';
+    console.log(address);
     $body.append('<img class="bgimg" src="'+ streetimgaddress + '">');
 
-    $greeting.text('So, you want to live at '+ address+ '?')
+    $greeting.text('So, you want to live at '+ address+ '?');
+    var NytQueryUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+
+        inputCity +'&sort=newest&api-key=ec612587cb260600bc67a560ab4342ef:8:71766984';
+    console.log(NytQueryUrl);
+    $.getJSON(NytQueryUrl, function(data){
+        console.log(data);
+    });
 
-    $.getJSON('http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ address +
-        '&apt-key=ec612587cb260600bc67a560ab4342ef:8:71766984', displayArticles(data));
-
-    //return false;
+    return false;
 };
 
 $('#form-container').submit(loadData);
